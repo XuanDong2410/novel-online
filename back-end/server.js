d5f7a1b308df8ee571aee3e99673bd5b87641464
XuanDong2410 <109146648+XuanDong2410@users.noreply.github.com>
2025-01-19 14:52:43 +0000

    add route and controller for tv show

--- back-end/server.js
+++ back-end/server.js
@@ -1,8 +1,9 @@
 import express from 'express'
 
 import authRoutes from "./routes/auth.route.js"
-import movieRoutes from "./routes/movie.route.js"
-
+//import movieRoutes from "./routes/movie.route.js"
+//import tvRoutes from "./routes/tv.route.js"
+import tmdbRoutes from "./routes/tmdb.route.js"
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
 
@@ -14,7 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", movieRoutes);
-
+app.use("/api/v1/tv", tvRoutes);
+//app.use("/api/v1/tmdb", tmdbRoutes);
 app.listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}`);
     connectDB();
