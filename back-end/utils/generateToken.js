f38a8ee8c1d4ccbbd284d577043d00e78146d4e0
XuanDong2410 <109146648+XuanDong2410@users.noreply.github.com>
2025-01-19 08:35:06 +0000

    generate jwt function

---
+++ back-end/utils/generateToken.js
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken';
+import { ENV_VARS } from '../config/envVars.js';
+
+export const generateTokenAndSetCookie = (userId, res) => {
+    const token = jwt.sign({ userId }, ENV_VARS.JWT_SECRET, { expiresIn: '7d' });
+    res.cookie('jwt-novel', token, {
+        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days in milliseconds
+        //expires: 15 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
+        httpOnly: true, // prevents client side JS from reading the cookie, 
+        // prevents XSS attacks cross-site scripting attacks, make it not be accessed by JS
+        sameSite: "strict", // cookie will only be sent in a first-party context
+        // CSRF attacks, protect against cross-site request forgery
+        secure: ENV_VARS.NODE_ENV === 'development', // cookie will only be sent in
+    });
+    return token;
+};
