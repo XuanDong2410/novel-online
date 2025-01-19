337814ff93f29087ab9dd6bc275381b6e41d5f51
XuanDong2410 <109146648+XuanDong2410@users.noreply.github.com>
2025-01-19 13:57:41 +0000

    add movie routes

--- back-end/server.js
+++ back-end/server.js
@@ -1,5 +1,8 @@
 import express from 'express'
+
 import authRoutes from "./routes/auth.route.js"
+import movieRoutes from "./routes/movie.route.js"
+
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
 
@@ -8,9 +11,12 @@ const PORT = ENV_VARS.PORT;
 
 app.use(express.json()); // Will parse incoming JSON requests
 app.use(express.urlencoded({ extended: true })); 
+
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", movieRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}`);
     connectDB();
 })
+
