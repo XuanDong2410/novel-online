c14e3d2d4b5737f9546371fc970763922e559581
XuanDong2410 <daoxuandong7012@gmail.com>
2025-01-20 16:19:40 +0700

    add server file for be

---
+++ back-end/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+import authRouters from './routes/auth.route.js';
+
+import { ENV_VARS } from './config/envVars.js';
+import { connectDB } from './config/db.js';
+
+
+const app = express(); 
+const PORT = ENV_VARS.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/V1/auth', authRouters);
+
+app.listen((PORT), () => {
+    console.log('Server is running on port 5000');
+    connectDB();
+})
