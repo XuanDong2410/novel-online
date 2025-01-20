da91990527fdde2469974025f0aeb22ab4717954
XuanDong2410 <daoxuandong7012@gmail.com>
2025-01-21 00:19:22 +0700

    add cloudinary variables in .env

--- back-end/config/envVars.js
+++ back-end/config/envVars.js
@@ -6,4 +6,7 @@ export const ENV_VARS = {
     MONGO_URI: process.env.MONGO_URI,
     PORT: process.env.PORT,
     JWT_SECRET: process.env.JWT_SECRET,
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
 }
