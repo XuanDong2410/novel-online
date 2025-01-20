8a146ccb944c3b5207f44d3b744fec6ef7616700
XuanDong2410 <daoxuandong7012@gmail.com>
2025-01-20 15:06:02 +0700

    add user model

---
+++ back-end/models/user.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    },
+}, {timestamps: true})
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
