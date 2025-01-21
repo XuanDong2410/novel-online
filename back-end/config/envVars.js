8bb4899e1b17b2d81af810726d170f2cd16a13aa
XuanDong2410 <daoxuandong7012@gmail.com>
2025-01-21 16:31:42 +0700

    add google api credentials

--- back-end/config/envVars.js
+++ back-end/config/envVars.js
@@ -1,6 +1,14 @@
 import dotenv from 'dotenv';
+import path from 'path';
+
 
 dotenv.config();
+
+// const googleCredentialsPath = path.resolve(
+//     process.cwd(), // Thư mục gốc nơi bạn chạy ứng dụng
+//     process.env.GOOGLE_APPLICATION_CREDENTIALS
+// );
+//process.env.GOOGLE_APPLICATION_CREDENTIALS = "/path/to/your/file.json";
 
 export const ENV_VARS = {
     MONGO_URI: process.env.MONGO_URI,
@@ -9,4 +17,5 @@ export const ENV_VARS = {
     CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
     CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
     CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+    GOOGLE_APPLICATION_CREDENTIALS: process.env.GOOGLE_APPLICATION_CREDENTIALS,
 }
