5f86ac5e1fb966e17208269b8f24b7a488eb5084
XuanDong2410 <109146648+XuanDong2410@users.noreply.github.com>
2025-01-22 09:19:33 +0000

    add func check user login or not

---
+++ back-end/middleware/protectRoute.js
@@ -0,0 +1,39 @@
+import jwt, { decode } from 'jsonwebtoken';
+import { ENV_VARS } from '../config/envVars.js';
+import { User } from '../models/user.model.js';
+
+export const protectRoute = async (req, res, next) => {
+    try {
+        const token = req.cookies["jwt-novel"];
+        if(!token) {
+            return res.status(401).json({
+                success: false,
+                message: "Unauthorized access - No Token Provided"});
+        }
+
+        const decoded = jwt.verify(token, ENV_VARS.JWT_SECRET);
+
+        if(!decoded) {
+            return res.status(401).json({
+                success: false,
+                message: "Unauthorized access - Invalid Token"});
+        }
+
+        const user = await User.findById(decoded.userId).select("-password");
+
+        if(!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found"});
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error: " + error.message
+        });
+        console.error("Error in protectRoute middleware: " + error.message);
+    }
+}
