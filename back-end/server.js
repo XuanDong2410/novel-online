d150a33e5e965fe4b32920a4a99b8b73d465cdae
XuanDong2410 <109146648+XuanDong2410@users.noreply.github.com>
2025-01-22 10:17:29 +0000

    add search route and controller

--- back-end/server.js
+++ back-end/server.js
@@ -4,7 +4,9 @@ import cookieParser from 'cookie-parser';
 import authRoutes from "./routes/auth.route.js"
 import movieRoutes from "./routes/movie.route.js"
 import tvRoutes from "./routes/tv.route.js"
+import searchRoutes from "./routes/search.route.js"
 //import tmdbRoutes from "./routes/tmdb.route.js"
+
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
 import { protectRoute } from './middleware/protectRoute.js';
@@ -20,6 +22,7 @@ app.use(cookieParser());
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
+app.use("/api/v1/search", protectRoute, searchRoutes);
 //app.use("/api/v1/tmdb", tmdbRoutes);
 app.listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}`);
