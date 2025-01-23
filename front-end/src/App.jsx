46dc831689d4646132fdb3c66ca75d57a9b6d6fb
XuanDong2410 <109146648+XuanDong2410@users.noreply.github.com>
2025-01-23 11:55:45 +0000

    use Auth Store in Page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,18 +1,36 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import HomePage from "./pages/home/HomePage"
 import SignUpPage from "./pages/SignUpPage"
 import LogInPage from "./pages/LogInPage"
 import Footer from "./components/Footer"
+import { Toaster } from "react-hot-toast"
+import { useAuthStore } from "./store/authUser"
+import { useEffect } from "react"
+import { Loader } from "lucide-react"
 
 function App() {
-
+  const { user, isCheckingAuth, authCheck } = useAuthStore()
+  console.log("Auth user is here: ", user) 
+  useEffect(() => {
+    authCheck()
+  }, [authCheck])
+  if(isCheckingAuth) {
+    return(
+      <div className="h-screen">
+        <div className="flex justify-center items-center bg-black h-full">
+          <Loader className="animate-spin text-red-600 size-10" />
+        </div>
+      </div>
+    )
+  }
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage/>} />
-        <Route path="/login" element={<LogInPage/>} />
-        <Route path="/signup" element={<SignUpPage/>} />
+        <Route path="/login" element={!user ? <LogInPage/> : <Navigate to={"/"} />} />
+        <Route path="/signup" element={!user ? <SignUpPage/> : <Navigate to={"/"} />} />
       </Routes>
+      <Toaster />
       <Footer />
     </>
   )
