bd17df5d5b6548c0e65b0b92075adc2d993d580d
XuanDong2410 <109146648+XuanDong2410@users.noreply.github.com>
2025-01-23 11:49:31 +0000

    add authCheck in Route

--- back-end/routes/auth.route.js
+++ back-end/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { login, logout, signup } from '../controllers/auth.controller.js';
+import { login, logout, signup, authCheck } from '../controllers/auth.controller.js';
+import { protectRoute } from '../middleware/protectRoute.js';
 
 const router = express.Router();
 
@@ -7,4 +8,5 @@ router.post("/signup", signup);
 router.post("/login", login)
 router.post("/logout", logout)
 
+router.get("/authCheck", protectRoute, authCheck);
 export default router;
