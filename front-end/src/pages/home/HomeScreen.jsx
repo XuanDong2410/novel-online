7fa5698fa52cfb40ec237719960ee1866352667c
XuanDong2410 <109146648+XuanDong2410@users.noreply.github.com>
2025-01-24 11:22:49 +0000

    add navbar in homescreen

--- front-end/src/pages/home/HomeScreen.jsx
+++ front-end/src/pages/home/HomeScreen.jsx
@@ -1,11 +1,13 @@
-import { useAuthStore } from "../../store/authUser"
-const HomeScreen = () => {
-  const { logout } = useAuthStore()
-  return (
-    <div>HomeScreen
+import Navbar from "../../components/Navbar"
 
-    <button onClick={logout}>Logout</button>
-    </div>
+
+const HomeScreen = () => {
+  return (
+    <>
+      <div className="relative h-screen text-white bg-black">
+        <Navbar />
+      </div>
+    </>
   )
 }
 
