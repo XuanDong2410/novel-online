1b086b715587f93f0da512347ff4339e67c3055c
XuanDong2410 <109146648+XuanDong2410@users.noreply.github.com>
2025-01-25 13:47:37 +0000

    add signing and logging when click button

--- front-end/src/pages/SignUpPage.jsx
+++ front-end/src/pages/SignUpPage.jsx
@@ -10,7 +10,7 @@ const SignUpPage = () => {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
 
-    const { signup } = useAuthStore()
+    const { signup, isSigningUp } = useAuthStore()
 
     const handleSignUp = (e) => {
         e.preventDefault()
@@ -69,10 +69,13 @@ const SignUpPage = () => {
                         />
                     </div>
 
-                    <button className="w-full py-2 bg-red-600 text-white font-semibold 
-                                    rounded-md hover:bg-red-700">
-                        Sign Up
-                    </button>
+                    <button
+							className='w-full py-2 bg-red-600 text-white font-semibold rounded-md
+							hover:bg-red-700'
+							disabled={isSigningUp}
+					>
+						{isSigningUp ? "Loading..." : "Sign Up"}
+					</button>
                 </form>
                 <div className="text-center text-gray-400">
                     Adready a member?{" "}
