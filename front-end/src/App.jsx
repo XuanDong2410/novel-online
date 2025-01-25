f4cabcf0324e3def4c93f7ec120f0be328e8ae47
XuanDong2410 <109146648+XuanDong2410@users.noreply.github.com>
2025-01-25 13:48:25 +0000

    add search history page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -9,7 +9,8 @@ import { useEffect } from "react"
 import { Loader } from "lucide-react"
 import WatchPage from "./pages/WatchPage"
 import SearchPage from "./pages/SearchPage"
-
+import SearchHistoryPage from "./pages/SearchHistoryPage"
+import NotFoundPage from "./pages/404"
 function App() {
   const { user, isCheckingAuth, authCheck } = useAuthStore()
   console.log("Auth user is here: ", user) 
@@ -34,6 +35,9 @@ function App() {
         <Route path="/watch/:id" element={user ? <WatchPage/> : <Navigate to={"/login"} />} />
         
         <Route path="/search" element={user ? <SearchPage/> : <Navigate to={"/login"} />} />
+        <Route path="/history" element={user ? <SearchHistoryPage/> : <Navigate to={"/login"} />} />
+
+        <Route path="/*" element={<NotFoundPage />} />
       </Routes>
       <Toaster />
       <Footer />
