a0415885777c0290afb5d39fdef0916fae591615
XuanDong2410 <109146648+XuanDong2410@users.noreply.github.com>
2025-01-25 04:01:45 +0000

    add sensitive file

---
+++ back-end/utils/sensitiveWords.js
@@ -0,0 +1,6 @@
+export const sensitiveWords = [
+    "tục tĩu", "bạo lực", "18+", "phản động", "xxx", "nhạy cảm",
+    "chửi thề", "đồi trụy", "dương vật", "lồn", "địt", "đéo", "đụ", "cặc", "lồn", "địt mẹ"
+  ];
+
+  
