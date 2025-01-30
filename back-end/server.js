9bcc4af218ad5f54b6dc7b7083f16c54349c7e97
XuanDong2410 <daoxuandong7012@gmail.com>
2025-01-30 17:13:41 +0700

    update: refactor server file

--- back-end/server.js
+++ back-end/server.js
@@ -1,26 +1,24 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import authRouters from './routes/auth.route.js';
-import novelRouters from './routes/novel/novel.route.js';
-import chapterRouters from './routes/novel/chapter.route.js';
-import validRouters from './routes/valid.route.js';
-import { ENV_VARS } from './config/envVars.js';
+
+import { createApp, PORT } from './config/app.config.js';
+import { configureRoutes } from './config/routes.config.js';
 import { connectDB } from './config/db.js';
 
 
-const app = express(); 
-const PORT = ENV_VARS.PORT;
+async function startServer() {
+    const app = createApp()
+    configureRoutes(app);
+    app.listen((PORT), () => {
+        console.log('Server is running on port 5000');
+        connectDB();
+    })
+}
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+process.on('unhandledRejection', (err) => {
+    console.error('Unhandled Rejection: ', err.message);
+    process.exit(1);
+});
 
-app.use('/api/V1/auth', authRouters);
-app.use('/api/V1/novel', novelRouters);
-app.use('/api/V1/chapter', chapterRouters);
-app.use('/api/V1/valid', validRouters);
-
-app.listen((PORT), () => {
-    console.log('Server is running on port 5000');
-    connectDB();
-})
+startServer().catch((err) => {
+    console.error('Error starting server: ', err.message);
+    process.exit(1);
+});
