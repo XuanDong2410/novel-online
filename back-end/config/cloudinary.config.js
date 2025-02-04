39c0991df1ee72b71afd10a48072eeabb877b513
XuanDong2410 <109146648+XuanDong2410@users.noreply.github.com>
2025-02-04 10:31:24 +0000

    create: new file to config cloudinary

---
+++ back-end/config/cloudinary.config.js
@@ -0,0 +1,12 @@
+// cloudinaryConfig.js
+import { v2 as cloudinary } from "cloudinary";
+import { ENV_VARS } from "../config/envVars.js";
+
+// Cloudinary configuration
+cloudinary.config({
+    cloud_name: ENV_VARS.CLOUDINARY_CLOUD_NAME,
+    api_key: ENV_VARS.CLOUDINARY_API_KEY,
+    api_secret: ENV_VARS.CLOUDINARY_API_SECRET,
+});
+
+export default cloudinary;
