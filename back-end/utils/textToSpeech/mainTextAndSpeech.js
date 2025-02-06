2efe8efb02d598166c4b2a879eec717e7ed7a361
XuanDong2410 <daoxuandong7012@gmail.com>
2025-02-06 10:26:22 +0000

    create: main func to auto generate audio and vtt file url

---
+++ back-end/utils/textToSpeech/mainTextAndSpeech.js
@@ -0,0 +1,16 @@
+import { processAudio } from "./processAudio.js";
+import { processVtt } from "./processVtt.js";
+
+export const generate = async (content, novelName, chapterName) => {
+    const voiceReq = {
+        "languageCodes": [
+            "vi-VN"
+        ],
+        "name": "vi-VN-Neural2-A",
+        "ssmlGender": "FEMALE",
+        "naturalSampleRateHertz": 24000
+    };
+    const audioFileUrl = await processAudio(content, voiceReq, novelName, chapterName);
+    const subtitleFileUrl = await processVtt(audioFileUrl, novelName, chapterName);
+    return { audioFileUrl, subtitleFileUrl };
+};
