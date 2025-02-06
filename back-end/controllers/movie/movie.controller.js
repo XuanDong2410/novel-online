e5d3240649604c47b27ea65cf450ef7b42037704
XuanDong2410 <daoxuandong7012@gmail.com>
2025-02-06 09:54:46 +0000

    delete: remove console.log(data)

--- back-end/controllers/movie/movie.controller.js
+++ back-end/controllers/movie/movie.controller.js
@@ -2,7 +2,7 @@ import { fetchFromTMDB } from '../../services/tmdb.service.js';
 export async function getTrendingMovie(req, res) {
     try {
         const data = await fetchFromTMDB("https://api.themoviedb.org/3/trending/movie/day?language=en-US");
-        console.log(data);
+        //console.log(data);
         const randomMovie = data.results[Math.floor(Math.random() * data.results?.length)];
 
         res.status(200).json({
