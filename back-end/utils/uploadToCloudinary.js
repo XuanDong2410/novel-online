9c74e221ce7516f28629e1dac8bd2d8db52c790d
XuanDong2410 <daoxuandong7012@gmail.com>
2025-02-06 10:03:07 +0000

    update: import func Format instead of create directly

--- back-end/utils/uploadToCloudinary.js
+++ back-end/utils/uploadToCloudinary.js
@@ -1,20 +1,6 @@
 import cloudinary from "../config/cloudinary.config.js";
 import fs from "fs/promises"; // Đúng cú pháp để dùng fs.unlink()
-import slugify from "slugify";
-
-// Hàm chuẩn hóa tên (loại bỏ dấu, khoảng trắng, viết thường)
-const formatName = (name) => {
-    // Kiểm tra nếu name không phải là chuỗi, mặc định cho nó là một chuỗi rỗng
-    if (typeof name !== "string") {
-        console.error("Warning: Invalid name provided, using empty string.");
-        name = "";
-    }
-    return slugify(name, {
-        lower: true, // Chuyển thành chữ thường
-        strict: true, // Loại bỏ ký tự đặc biệt
-        locale: "vi" // Hỗ trợ chuẩn hóa tiếng Việt
-    }).replace(/-+/g, "-"); // Loại bỏ dấu "-" dư thừa;
-};
+import { formatName } from "./formatName.js";
 
 // Hàm upload file lên Cloudinary
 const uploadToCloudinary = async (filePath, novelName, chapterName, fileName, options) => {
