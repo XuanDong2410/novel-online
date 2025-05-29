43d47b1a8e80adce62e8112c0c08326086f1bbad
XuanDong <daoxuandong7012@gmail.com>
2025-05-29 15:38:10 +0700

    update: add route for admin, moderator & notification

--- back-end/config/routes.config.js
+++ back-end/config/routes.config.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { isAdmin, isModerator } from '../middleware/isAdmin.js';
 import { protectRoute } from '../middleware/protectRoute.js';
 
 import authRouters from '../routes/auth.route.js';
@@ -8,16 +9,29 @@ import movieRouters from '../routes/movie/movie.route.js';
 import tvRouters from '../routes/movie/tv.route.js';
 import searchRouters from '../routes/movie/search.route.js';
 import audioRouters from '../routes/audio.route.js';
-// import validRouters from '../routes/valid.route.js';
+
+import notificationRouters from '../routes/notification.route.js';
+
+import adminRouters from '../routes/admin/admin.route.js';
+import moderatorRouters from '../routes/admin/moderator.route.js';
 
 export function configureRoutes(app) {
     app.use(express.json());
+
+    // Auth và public routes
     app.use('/api/v1/auth', authRouters);
+
+    // Người dùng bình thường
     app.use('/api/v1/audio', audioRouters);
     app.use('/api/v1/movie', protectRoute, movieRouters);
     app.use('/api/v1/novel', protectRoute, novelRouters);
-    app.use('/api/v1/chapter',protectRoute, chapterRouters);
+    app.use('/api/v1/chapter', protectRoute, chapterRouters);
     app.use('/api/v1/tv', protectRoute, tvRouters);
     app.use('/api/v1/search', protectRoute, searchRouters);
-//   app.use('/api/v1/valid', validRouters);
+
+    // Notification routes
+    app.use('/api/v1/notification', protectRoute, notificationRouters);
+    // Admin routes
+    app.use('/api/v2/admin', protectRoute, isAdmin, adminRouters);
+    app.use('/api/v2/admin', protectRoute, isModerator, moderatorRouters);
 }
