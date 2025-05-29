81851aeb3661a44f48c238f5f0f95469d730abb3
XuanDong <daoxuandong7012@gmail.com>
2025-05-29 15:39:41 +0700

    create: notification schema

---
+++ back-end/models/notification.model.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const notificationSchema = mongoose.Schema({
+    from: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    to: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true,
+        enum: [
+            'favorite',
+            'rate',
+            'comment',
+            'mention',
+            'reply',
+            'report',
+            'novelUpdate',
+            'adminNotice'
+        ]
+    },
+    read: {
+        type: Boolean,
+        default: false
+    }
+}, {timestamps: true});
+
+const Notification = mongoose.model('Notification', notificationSchema);
+
+export default Notification;
