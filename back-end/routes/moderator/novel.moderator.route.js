94bed96a323fa7230df4b7ad01bfcdd48eba1e85
XuanDong <daoxuandong7012@gmail.com>
2025-05-30 17:29:06 +0700

    create: create route novel by moderator

--- back-end/routes/moderator/novel.moderator.route.js
+++ back-end/routes/moderator/novel.moderator.route.js
@@ -1,15 +1,12 @@
 import express from 'express';
 import { isModerator } from '../../middleware/isAdmin.js';
 import { protectRoute } from '../../middleware/protectRoute.js';
-
-// Ví dụ: import các controller admin
-// import { getAllUsers, getSystemStats } from '../../controllers/admin.controller.js';
+import { moderateNovel } from '../../controllers/moderator/novel.moderator.controller.js';
 
 const router = express.Router();
 
 router.use(protectRoute, isModerator);
-
-// router.get('/users', getAllUsers);
+router.post('/:id/moderate', moderateNovel);
 // router.get('/stats', getSystemStats);
 
 export default router;
