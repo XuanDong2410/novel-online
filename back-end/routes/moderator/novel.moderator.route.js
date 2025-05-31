05b61d139d01637cc08ee46d3271f8c1a046dbb8
XuanDong2410 <daoxuandong7012@gmail.com>
2025-06-01 01:30:29 +0700

    update: route moderate action for mod

--- back-end/routes/moderator/novel.moderator.route.js
+++ back-end/routes/moderator/novel.moderator.route.js
@@ -1,12 +1,24 @@
 import express from 'express';
 import { isModerator } from '../../middleware/isAdmin.js';
 import { protectRoute } from '../../middleware/protectRoute.js';
-import { moderateNovel } from '../../controllers/moderator/novel.moderator.controller.js';
+import { 
+    getPendingNovels,
+    getNovelWithChapters,
+    getChapterDetails,
+    approveNovelAndChapters,
+    requestChapterEdit,
+    warnChapterViolation,
+} from '../../controllers/moderator/novel.moderator.controller.js';
 
 const router = express.Router();
 
 router.use(protectRoute, isModerator);
-router.post('/:id/moderate', moderateNovel);
-// router.get('/stats', getSystemStats);
+router.get('/', getPendingNovels);
+router.get('/:id', getNovelWithChapters);
+router.get('/:id/chapters/:chapterId', getChapterDetails);
+router.post('/:id/approve', approveNovelAndChapters);
+router.post('/:id/chapters/:chapterId/edit', requestChapterEdit);
+router.post('/:id/chapters/:chapterId/warn', warnChapterViolation);
+// router.post('/:id/chapters/:chapterId/flag', flagChapter);
 
 export default router;
