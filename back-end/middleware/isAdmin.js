32e7e2e47ba4a2826aee2b465eb2ad33cf39e05d
XuanDong2410 <daoxuandong7012@gmail.com>
2025-06-01 01:27:58 +0700

    update: update role system in middleware

--- back-end/middleware/isAdmin.js
+++ back-end/middleware/isAdmin.js
@@ -8,7 +8,7 @@ export const isAdmin = (req, res, next) => {
   next();
 };
 export const isModerator = (req, res, next) => {
-  if (!["moderator", "admin"].includes(req.user?.role)) {
+  if (!["moderator","system", "admin"].includes(req.user?.role)) {
     return res.status(403).json({
       success: false,
       message: "Access denied - Moderator or Admin only"
