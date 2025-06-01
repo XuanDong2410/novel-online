c7a89ed915e3740028240170aea1ed7dcb41c7ce
XuanDong2410 <daoxuandong7012@gmail.com>
2025-06-02 02:52:09 +0700

    update: remove to admin log

--- back-end/routes/moderator/log.moderator.route.js
+++ back-end/routes/moderator/log.moderator.route.js
@@ -1,20 +1,12 @@
 import express from 'express';
-import { isModerator, isAdmin } from '../../middleware/isAdmin.js';
-import { protectRoute } from '../../middleware/protectRoute.js';
 import { 
-    getAllModerationLogs,
-    getModerationLogById,
-    deleteModerationLogById,
-    deleteAllModerationLogs
+    viewAllMyModerationLogs,
+    viewMyModerationLogDetailById
 } from '../../controllers/moderator/log.moderator.controller.js';
 
 const router = express.Router();
 
-router.use(protectRoute, isModerator);
-
-router.get('/', protectRoute, isModerator, getAllModerationLogs);
-router.get('/:id', protectRoute, isModerator, getModerationLogById);
-router.delete('/:id', protectRoute, isAdmin, deleteModerationLogById);
-router.delete('/',protectRoute, isAdmin, deleteAllModerationLogs);
+router.get('/', viewAllMyModerationLogs);
+router.get('/:logId', viewMyModerationLogDetailById);
 
 export default router;
