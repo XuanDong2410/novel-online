931649d104cc13079a7f4466b3e1ae0e362f0a2e
XuanDong2410 <daoxuandong7012@gmail.com>
2025-06-02 00:50:23 +0700

    update: Add necessary routes for user actions with chapter

--- back-end/routes/user/chapter.user.route.js
+++ back-end/routes/user/chapter.user.route.js
@@ -1,9 +1,31 @@
 import express from "express";
-import { createChapter } from "../../controllers/user/chapter.user.controller.js";
-import { protectRoute } from "../../middleware/protectRoute.js";
+import { 
+    createChapter,
+    viewMyChapters,
+    viewMyChapterById,
+    updateChapter,
+    deleteChapter, 
+    requestPublish,
+    cancelRequestPublish,
+    resubmitChapter,
+    hideChapter,
+} from "../../controllers/user/chapter.user.controller.js";
 
 const router = express.Router();
 
-router.post("/create", protectRoute, createChapter);
+router.post("/", createChapter);
+
+router.get('/', viewMyChapters);
+router.get('/:id', viewMyChapterById);
+
+router.patch('/:id', updateChapter);
+
+router.delete('/:id', deleteChapter);
+
+router.post("/:id/request-publish", requestPublish);
+router.patch("/:id/cancel-request", cancelRequestPublish);
+router.patch("/:id/resubmit", resubmitChapter);
+
+router.patch("/:id/hide", hideChapter);
 
 export default router;
