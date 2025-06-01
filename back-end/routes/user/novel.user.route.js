f31105e15a4d110e46f859fe95f7a7c88a4c0b72
XuanDong2410 <daoxuandong7012@gmail.com>
2025-06-02 00:36:50 +0700

    update: Add necessary action routes for users

--- back-end/routes/user/novel.user.route.js
+++ back-end/routes/user/novel.user.route.js
@@ -1,9 +1,35 @@
 import express from "express";
-import { createNovel, requestPublish } from "../../controllers/user/novel.user.controller.js";
-import { protectRoute } from "../../middleware/protectRoute.js";
+import { 
+    createNovel,
+    viewMyNovels,
+    viewMyNovelById,
+    updateNovel,
+    updateNovelCover,
+    deleteNovel, 
+    requestPublish,
+    cancelRequestPublish,
+    resubmitNovel,
+    hideNovel,
+    getNovelStats,
+} from "../../controllers/user/novel.user.controller.js";
 
 const router = express.Router();
 
-router.post("/create", protectRoute, createNovel);
-router.post("/:id/request-publish", protectRoute, requestPublish);
+router.post("/", createNovel);
+
+router.get('/', viewMyNovels);
+router.get('/:id', viewMyNovelById);
+
+router.patch('/:id', updateNovel);
+router.patch('/:id/cover', updateNovelCover);
+
+router.delete('/:id', deleteNovel);
+
+router.post("/:id/request-publish", requestPublish);
+router.patch("/:id/cancel-request", cancelRequestPublish);
+router.patch("/:id/resubmit", resubmitNovel);
+
+router.patch("/:id/hide", hideNovel);
+router.get('/:id/stats', getNovelStats);
+
 export default router;
