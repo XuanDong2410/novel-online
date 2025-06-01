2a57469c6e0d1417e21dd79c71aa9ba80b307ea8
XuanDong2410 <daoxuandong7012@gmail.com>
2025-06-02 02:46:29 +0700

    update: remove middleware unecessary

--- back-end/routes/moderator/report.moderator.route.js
+++ back-end/routes/moderator/report.moderator.route.js
@@ -5,15 +5,13 @@ import {
   getReportById,
   handleReport
 } from "../../controllers/moderator/report.moderator.controller.js";
-import { isModerator } from '../../middleware/isAdmin.js';
-import { protectRoute } from '../../middleware/protectRoute.js';
 
 const router = express.Router();
 
 // Mod lấy danh sách report
-router.get("/", protectRoute, isModerator, getPendingReports); 
-router.get("/handled", protectRoute, isModerator, getHandledReports); // Mod lấy danh sách report đã xử lý
-router.get("/:reportId", protectRoute, isModerator, getReportById);  // Mod xem chi tiết
-router.patch("/:reportId/handle", protectRoute, isModerator, handleReport); // Mod xử lý
+router.get("/", getPendingReports); 
+router.get("/handled", getHandledReports); // Mod lấy danh sách report đã xử lý
+router.get("/:reportId", getReportById);  // Mod xem chi tiết
+router.patch("/:reportId/handle", handleReport); // Mod xử lý
 
 export default router;
