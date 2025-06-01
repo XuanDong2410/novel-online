41adad0be27f5531d39474cd3dc9e18448b61839
XuanDong2410 <daoxuandong7012@gmail.com>
2025-06-02 00:35:15 +0700

    delete: unecessary file

--- back-end/routes/valid.route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { validate } from '../controllers/vertification/vertification.controller.js';
-
-const router = express.Router();
-
-router.post('/validate', validate);
-
-export default router;
