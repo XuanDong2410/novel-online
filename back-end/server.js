f13493a2f4dc94b4d8210996cd944f8d875f6739
XuanDong <daoxuandong7012@gmail.com>
2025-06-02 12:09:11 +0700

    update: add socket when start server

--- back-end/server.js
+++ back-end/server.js
@@ -1,26 +1,32 @@
-
-import { createApp, PORT } from './config/app.config.js';
-import { configureRoutes } from './config/routes.config.js';
-import { connectDB } from './config/db.config.js';
-import { initializeSystemUser } from './config/systemAccount.config.js';
-
+import { createApp, PORT } from "./config/app.config.js";
+import { configureRoutes } from "./config/routes.config.js";
+import { connectDB } from "./config/db.config.js";
+import { initializeSystemUser } from "./config/systemAccount.config.js";
+import http from "http";
+import { setupWebSocket } from "./config/webSocket.config.js";
 
 async function startServer() {
-    const app = createApp()
-    configureRoutes(app);
-    connectDB();
-    initializeSystemUser();
-    app.listen((PORT), () => {
-        console.log(`Server is running on port ${PORT}`);
-    })
+  const app = createApp();
+  configureRoutes(app);
+  const server = http.createServer(app);
+
+  // Khởi tạo socket
+  setupWebSocket(server);
+
+  connectDB();
+  initializeSystemUser();
+  
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 }
 
-process.on('unhandledRejection', (err) => {
-    console.error('Unhandled Rejection: ', err.message);
-    process.exit(1);
+process.on("unhandledRejection", (err) => {
+  console.error("Unhandled Rejection: ", err.message);
+  process.exit(1);
 });
 
 startServer().catch((err) => {
-    console.error('Error starting server: ', err.message);
-    process.exit(1);
+  console.error("Error starting server: ", err.message);
+  process.exit(1);
 });
