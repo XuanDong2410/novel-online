3656d842396533c78dc4266a22acb789cce77106
XuanDong <daoxuandong7012@gmail.com>
2025-06-02 12:04:46 +0700

    update: add new secret jwt refresh key and allowed origins

--- back-end/config/env.config.js
+++ back-end/config/env.config.js
@@ -4,15 +4,23 @@ dotenv.config();
 
 export const ENV_VARS = {
     MONGO_URI: process.env.MONGO_URI,
+
     PORT: process.env.PORT,
     JWT_SECRET: process.env.JWT_SECRET,
+    JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
+    ALLOWED_ORIGINS: process.env.ALLOWED_ORIGINS,
+
     TMDB_API_KEY: process.env.TMDB_API_KEY,
+
     CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
     CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
     CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+
     GOOGLE_APPLICATION_CREDENTIALS: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
     NOVEL_OPENAI_API_KEY: process.env.NOVEl_OPENAI_API_KEY,
+    
     RATE_LIMIT: 10,
     SYSTEM_USER_PASSWORD:  process.env.SYSTEM_USER_PASSWORD || 'NovelOnlinePlatform',
 }
