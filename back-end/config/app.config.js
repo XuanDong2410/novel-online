4f18bcbdcdec32f4dd05bd0e828fedb358ac86f2
XuanDong2410 <daoxuandong7012@gmail.com>
2025-06-05 01:12:08 +0700

    update: use cors to connect be with fe

--- back-end/config/app.config.js
+++ back-end/config/app.config.js
@@ -1,11 +1,18 @@
 import express from "express"
+import cors from 'cors'
 import cookieParser from "cookie-parser"
 import bodyParser from "body-parser"
 import { ENV_VARS } from "./env.config.js"
 
-export function createApp(){
+export function createApp() {
     const app = express()
-
+    // Cấu hình CORS
+    app.use(cors({
+        origin: 'http://localhost:3000', // Cho phép frontend từ localhost:3000
+        credentials: true, // Cho phép gửi cookie
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Các phương thức HTTP được phép
+        allowedHeaders: ['Content-Type', 'Authorization'] // Các header được phép
+    }))
     app.use(express.json())
     app.use(bodyParser.urlencoded({ extended: true }))
     app.use(cookieParser())
